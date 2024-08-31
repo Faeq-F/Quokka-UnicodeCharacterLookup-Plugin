f157959ed3a0f8ad2f9e7ccb677012b9ad70d030
Faeq <faeqfaisal@hotmail.co.uk>
2024-08-31 18:15:40 +0100

    :sparkles: implemented plugin, command specifier and specialCommand

--- Plugin_UnicodeCharacterLookup.cs
+++ Plugin_UnicodeCharacterLookup.cs
@@ -1,6 +1,98 @@
 
+using Newtonsoft.Json;
+using Quokka.ListItems;
+using Quokka.PluginArch;
+using System.IO;
+using WinCopies.Util;
+
 namespace Plugin_UnicodeCharacterLookup {
-  public class UnicodeCharacterLookup {
+
+  /// <summary>
+  /// The Unicode Character Lookup plugin
+  /// </summary>
+  public class UnicodeCharacterLookup : Plugin {
+
+    internal List<CharItem> AllChars = new();
+    private static PluginSettings pluginSettings = new();
+    internal static PluginSettings PluginSettings { get => pluginSettings; set => pluginSettings = value; }
+
+    /// <summary>
+    /// Loads plugin settings
+    /// </summary>
+    public UnicodeCharacterLookup() {
+      string fileName = Environment.CurrentDirectory + "\\PlugBoard\\Plugin_UnicodeCharacterLookup\\Plugin\\settings.json";
+      PluginSettings = JsonConvert.DeserializeObject<PluginSettings>(File.ReadAllText(fileName))!;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public override string PluggerName { get; set; } = "UnicodeCharacterLookup";
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// Loads the UnicodeChars.json file
+    /// </summary>
+    public override void OnAppStartup() {
+      string fileName = Environment.CurrentDirectory + "\\PlugBoard\\Plugin_UnicodeCharacterLookup\\Plugin\\UnicodeChars.json";
+      List<Char> chars = JsonConvert.DeserializeObject<List<Char>>(File.ReadAllText(fileName))!;
+      foreach (Char i in chars) AllChars.Add(new CharItem(i));
+    }
+
+    private List<ListItem> ProduceItems(string query) {
+      List<ListItem> IdentifiedChars = new();
+      //filtering apps
+      foreach (CharItem ch in AllChars) {
+        if (
+            ch.Name.Contains(query, StringComparison.OrdinalIgnoreCase)
+            || ( FuzzySearch.LD(ch.Description, query) < PluginSettings.FuzzySearchThreshold )
+        ) {
+          IdentifiedChars.Add(ch);
+        }
+      }
+      return IdentifiedChars;
+    }
+
+    public override List<ListItem> OnQueryChange(string query) {
+      return ProduceItems(query);
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns>The AllCharsSpecialCommand from plugin settings</returns>
+    public override List<String> SpecialCommands() {
+      List<String> SpecialCommand = new() { PluginSettings.AllCharsSpecialCommand };
+      return SpecialCommand;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="command">The AllCharsSpecialCommand from plugin settings (Since there is only 1 special command for this plugin)</param>
+    /// <returns>All Unicode characters</returns>
+    public override List<ListItem> OnSpecialCommand(string command) {
+      return new(AllChars);
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns>
+    /// The CharacterSignifier from plugin settings
+    /// </returns>
+    public override List<string> CommandSignifiers() {
+      return new List<string>() { PluginSettings.CharacterSignifier };
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="command">The CharacterSignifier (Since there is only 1 signifier for this plugin), followed by the character being searched for</param>
+    /// <returns>List of characters that possibly match what is being searched for</returns>
+    public override List<ListItem> OnSignifier(string command) {
+      return ProduceItems(command.Substring(PluginSettings.CharacterSignifier.Length));
+    }
   }
 
 }
--- Settings.cs
+++ Settings.cs
@@ -2,10 +2,22 @@
 /// All plugin specific settings
 /// </summary>
 public class PluginSettings {
-
   /// <summary>
-  /// The command signifier used to obtain the denotation of a word (defaults to "define ")<br />
-  /// Using this signifier is the only way to get a result from this plugin, as to not needlessly send queries to the dictionary API
+  /// The command signifier used to only show items from this plugin (defaults to "char ")<br />
+  /// Using this signifier does not change the output of this plugin, it only
+  /// ensures that no other plugins' results are included in the search window results list
   /// </summary>
-  public string DictionarySignifier { get; set; } = "define ";
+  public string CharacterSignifier { get; set; } = "char ";
+  /// <summary>
+  /// The command to show all Unicode characters
+  /// </summary>
+  public string AllCharsSpecialCommand { get; set; } = "AllChars";
+  /// <summary>
+  ///   The threshold for when to consider a character's description
+  ///   is similar enough to the query for it to be
+  ///   displayed (defaults to 5). Currently uses the
+  ///   Levenshtein distance; the larger the number, the
+  ///   bigger the difference.
+  /// </summary>
+  public int FuzzySearchThreshold { get; set; } = 5;
 }
