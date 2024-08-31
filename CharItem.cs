b8e7c2df86f8ee7c65f778f6135425261658982b
Faeq <faeqfaisal@hotmail.co.uk>
2024-08-31 17:45:29 +0100

    :construction: created the list item type for characters

---
+++ CharItem.cs
@@ -0,0 +1,22 @@
+﻿using Quokka;
+using Quokka.ListItems;
+using System.Windows.Media.Imaging;
+
+namespace Plugin_UnicodeCharacterLookup {
+  class CharItem : ListItem {
+
+    public CharItem(Char character) {
+      this.Name = character.character;
+      this.Description = character.description;
+      this.Icon = new BitmapImage(new Uri(
+          Environment.CurrentDirectory + "\\PlugBoard\\Plugin_UnicodeCharacterLookup\\Plugin\\globe.png"));
+    }
+
+    //When item is selected, copy text
+    public override void Execute() {
+      System.Windows.Clipboard.SetText(Name);
+      App.Current.MainWindow.Close();
+    }
+  }
+
+}
