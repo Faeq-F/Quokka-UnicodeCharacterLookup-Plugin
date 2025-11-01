f79684aefa661476632a6c67f64878b088ccbb5b
Faeq <faeqfaisal@hotmail.co.uk>
2025-11-01 10:59:22 +0000

    chore: :alien: using UI Dispatcher to create Icon for items

--- CharItem.cs
+++ CharItem.cs
@@ -1,15 +1,18 @@
 ﻿using Quokka;
 using Quokka.ListItems;
+using Quokka.PluginArch;
 using System.Windows.Media.Imaging;
 
 namespace Plugin_UnicodeCharacterLookup {
   class CharItem : ListItem {
 
     public CharItem(string character, string name) {
-      this.Name = character;
-      this.Description = name;
-      this.Icon = new BitmapImage(new Uri(
-          Environment.CurrentDirectory + "\\PlugBoard\\Plugin_UnicodeCharacterLookup\\Plugin\\globe.png"));
+      Name = character;
+      Description = name;
+      UiDispatcher.BeginInvoke(() => {
+        Icon = new BitmapImage(new Uri(
+            Environment.CurrentDirectory + "\\PlugBoard\\Plugin_UnicodeCharacterLookup\\Plugin\\globe.png"));
+      });
     }
 
     //When item is selected, copy text
